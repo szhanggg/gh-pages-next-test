5f98531b79acfd65a5d4f553a05addbb7c0573b8
grififth <stevez9101@gmail.com>
2022-10-06 22:27:43 -0400

    o

--- pages/bro.js
+++ pages/bro.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Bro = () => {
-  return <div>bro?</div>;
+const Bro = ({ data }) => {
+    return <div>{data}</div>;
 };
+
+export async function getServerSideProps() {
+    const res = await fetch("http://159.223.177.167:3000/test/test");
+    const data = await res.text();
+
+    return {
+        props: {
+            data,
+        },
+    };
+}
 
 export default Bro;
